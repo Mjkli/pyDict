5828c5cdd5ac924c0d0f9e3df50fec0bf6b4d0c6
Anthony <anthony.bruno@automotivepower.com>
2021-09-10 18:07:22 -0700

    building basic dictonary structure

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 
 import requests
 
-class Hashtable:
-    
-    def __init__(self) -> None:
-        pass
 
+Words = {}
 
+Words["test"] = ["Noun",
+        "something (such as a series of questions or exercises) for measuring the skill, knowledge, intelligence, capacities, or aptitudes of an individual or group"]
+Words["fulminate"] = ["verb",
+            "to send forth harsh criticisms or insults"]
+
+print(Words["test"])
 
 
 
